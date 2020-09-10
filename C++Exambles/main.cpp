beb6f39e36effd5f692c50996e750cd99ca90350
Mohamed osama <mohamed.osama9812@gmail.com>
2020-09-09 17:17:03 -0700

    Type Casting

--- C++Exambles/main.cpp
+++ C++Exambles/main.cpp
@@ -7,9 +7,16 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    
+    int a = 12 , b = 7 ;
+    
+    float result = (float) a / b ;
+    
+    cout<< result << endl;
+    
     return 0;
 }
